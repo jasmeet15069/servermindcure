072b6a2d40dc10eb8a5eca10aae95de3b7666003
Jasmeet Sethi <142715156+jasmeet15069@users.noreply.github.com>
2024-09-08 13:18:25 +0530

    Connection from mongodb db.js
    remove localhost link

--- server/config/db.js
+++ server/config/db.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const connectDb = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb://localhost:27017"
-      // "mongodb+srv://admin:jasmeet@cluster0.rgkg3r1.mongodb.net/moodjournal?retryWrites=true&w=majority"
+     // "mongodb://localhost:27017"
+      "mongodb+srv://admin:jasmeet@cluster0.rgkg3r1.mongodb.net/moodjournal?retryWrites=true&w=majority"
     );
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
